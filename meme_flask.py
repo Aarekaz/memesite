b51c4641631062f9cb047ec6bb55d607a6161e6a
Anurag Dhungana <36888347+Aarekaz@users.noreply.github.com>
2022-11-11 22:35:23 +0545

    fixed stuffs

--- meme_flask.py
+++ meme_flask.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import json
 
-app = Flask(__name__)   
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
-@app.route('/')
+def get_meme():
+    # Uncomment these two lines and comment out the other url line if you want to use a specific meme subreddt
+    # sr = "/wholesomememes"
+    # url = "<https://meme-api.herokuapp.com/gimme>" + sr
+    url = "<https://meme-api.herokuapp.com/gimme>"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
 
+
+@app.route("/")
 def index():
-    return '<h1>Hello World!</h1>'
+    meme_pic, subreddit = get_meme()
+    return render_template("index.html", meme_pic=meme_pic, subreddit=subreddit)
 
-app.run(host="0.0.0.0" , port = 3000)
+
+app.run(host="0.0.0.0", port=81)
